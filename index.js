83ada955b0876298a756751c7420441b80e951ec
MarcelloDev <marcelodevgithub@gmail.com>
2025-08-22 10:58:36 -0300

    usando post

--- index.js
+++ index.js
@@ -3,13 +3,20 @@ import { memo } from "react";
 
 const app = express()
 
+app.use(express.json())
+
 const user = []
 
 //ROTAS
 
 app.get("/users", (request, response)=>{
-    return response.json({mensagem: "Deu certo"})
-    
+    return response.json(user)
+
 });
+
+app.post("/users", (request, response)=> {
+    console.log(request)
+    return response.json({ ok: true })
+})
 
 app.listen(3000)
