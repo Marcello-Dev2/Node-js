76c4a6b8bc4ce0cd02f503ff0aa31d1da27a3552
MarcelloDev <marcelodevgithub@gmail.com>
2025-08-22 09:47:41 -0300

    npm

--- sintaxe/index.js
+++ sintaxe/index.js
@@ -4,12 +4,14 @@ const server = express()
 
 //ROTAS
 server.get("/abacate", (request, response) => {
+    
     return response.send("Esse é seu abacate")
 })
 
 server.get("/morango", (request, response) => {
-    console.log(request)
+   
     return response.send("Esse é seu morango")
 })
 
 server.listen(3000)
+
