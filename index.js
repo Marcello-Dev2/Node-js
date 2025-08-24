5181a6e15baa4f38e0793dde75440188afaf24c6
MarcelloDev <marcelodevgithub@gmail.com>
2025-08-24 11:19:27 -0300

    achar o erro do insomnia

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import User from "./models/user.js";
 
 const app = express()
 
@@ -13,22 +14,20 @@ app.get("/users", (request, response) => {
     return response.json(users)
 })
 
-app.post("/users", (request, response) => {
+app.post("/users", async (request, response) => {
+    const user = request.body
 
-    const name = request.body.name;
-    const age = request.body.age;
-    const nickName = request.body.nick;
+    const newUser= await User.create(user)
 
-    users.push({ name , age , nickName });
 
-    return response.json({ name , age , nickName });
+    return response.json(newUser);
 });
 
 
 mongoose.connect
-('mongodb+srv://Marcello:7zPjfLjvITB9OwCJ@cluster0.035pzka.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    ('mongodb+srv://Marcello:7zPjfLjvITB9OwCJ@cluster0.035pzka.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 
-.then(() => console.log("Banco de dados conectar"))
-.catch(() => console.log("Deu erro"))
+    .then(() => console.log("Banco de dados conectar"))
+    .catch(() => console.log("Deu erro"))
 
 app.listen(3000);
--- models/user.js
+++ models/user.js
@@ -1,12 +1,19 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    name : {
-        type : String,
+    name: {
+        type: String,
         required: true
     },
-    age:{
-        type : Number,
-        required : true
+    age: {
+        type: Number,
+        required: true
+    },
+    createadAt: {
+        type: Date,
+        default: Date.now()
     }
 })
+
+
+export default mongoose.model("User", userSchema)
